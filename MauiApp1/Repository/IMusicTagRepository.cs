08477283afe01dce256e70d77e069ffd83cc16e4
Cseke <eu@lamine.com>
2024-04-15 10:35:16 +0300

    created interface for mtrepo

---
+++ MauiApp1/Repository/IMusicTagRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Repository
+{
+    public interface IMusicTagRepository
+    {
+        void add(MusicTag elem);
+        MusicTag? search(int id);
+        MusicTag[] getAll();
+    }
+}

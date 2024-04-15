7e1475c349c1daa0f754c768241dadaa8778108e
Cseke <eu@lamine.com>
2024-04-15 14:25:31 +0300

    basic repositories

---
+++ MauiApp1/Repository/ISongRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Repository
+{
+    internal interface ISongRepository
+    {
+        void add(Song elem);
+        void delete(Song elem);
+        Song? search(int id);
+        Song[] getAll();
+    }
+}
--- MauiApp1/Repository/TrackRepository.cs
+++ MauiApp1/Repository/TrackRepository.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace MauiApp1.Repository
 {
-    internal class TrackRepository
+    public class TrackRepository : ITrackRepository
     {
+        private List<Track> tracks;
+
+        public TrackRepository()
+        {
+            tracks = new List<Track>();
+        }
+
+        public void add(Track elem)
+        {
+            tracks.Add(elem);
+        }
+
+        public void delete(Track elem)
+        {
+            if (!tracks.Contains(elem))
+                throw new Exception("Element does not exist");
+            tracks.Remove(elem);
+        }
+
+        public Track? search(int id)
+        {
+            return (from track in tracks
+                    where track.id == id
+                    select track).Take(1); //?
+        }
+
+        public Track[] getAll()
+        {
+            return (from track in tracks
+                    select track).ToArray();
+        }
     }
 }

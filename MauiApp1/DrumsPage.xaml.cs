8ba40d357da6a7ef1a3979c21162c9c7406d4893
Fuziune <ioan.fusarau@stud.ubbcluj.ro>
2024-04-16 13:13:40 +0300

    Beat-drum-1-implewmentation Functioneaza soundul la un beat de toba

--- MauiApp1/DrumsPage.xaml.cs
+++ MauiApp1/DrumsPage.xaml.cs
@@ -1,10 +1,15 @@
+using Plugin.Maui.Audio;
+
 namespace MusicCreator;
 
 public partial class DrumsPage : ContentPage
 {
-	public DrumsPage()
+    private readonly IAudioManager audioManager;
+
+    public DrumsPage(IAudioManager audioManager)
 	{
 		InitializeComponent();
+        this.audioManager = audioManager;
 	}
     private async void OnNextClicked(object sender, EventArgs e)
     {
@@ -13,5 +18,11 @@ public partial class DrumsPage : ContentPage
     private async void OnBackClicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("Main");
+    }
+    private async void Drum1Clicked(object sender, EventArgs e)
+    {
+        var player = audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync("Bass-Drum-1.wav"));
+        player.Play();
+        //await Shell.Current.GoToAsync("Main");
     }
 }

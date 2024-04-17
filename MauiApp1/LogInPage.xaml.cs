678ebd60fb268c797c924fda084fc59627cbf770
912-Cheres-Andreea <115451911+cheresandreea@users.noreply.github.com>
2024-04-17 15:45:09 +0300

    UPDATED

--- MauiApp1/LogInPage.xaml.cs
+++ MauiApp1/LogInPage.xaml.cs
@@ -10,7 +10,16 @@
 
         private async void OnLoginClicked(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync("Main");
+            if (!string.IsNullOrWhiteSpace(UsernameEntry.Text) && !string.IsNullOrWhiteSpace(PasswordEntry.Text))
+            {
+                // Both fields are not empty, proceed with login
+                await Shell.Current.GoToAsync("Main");
+            }
+            else
+            {
+                // Show an alert indicating that both fields are required
+                await DisplayAlert("Error", "Username and password are required fields", "OK");
+            }
         }
 
         private async void OnForgotPasswordClicked(object sender, EventArgs e)
